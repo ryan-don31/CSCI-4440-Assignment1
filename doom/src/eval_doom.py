fc4563a331b3ebf49866927b2145794c043288c2
Sid-26 <siddhantdas262@gmail.com>
2025-10-28 02:48:48 -0400

    added evaluation code decoupled

--- doom/src/eval_doom.py
+++ doom/src/eval_doom.py
@@ -1,21 +1,37 @@
-import argparse
 import time
 import os
+import yaml
+import sys
 from stable_baselines3 import PPO, A2C
-from vizdoom import ScreenResolution
+sys.path.append('..') # make sure files dont violate this convention
 from envs.doom_env import VizDoomGym, VizDoomGymCorridor
 
-
 # Base directories
-MODEL_BASE_DIR = "../models/"
-SCENARIO_BASE_DIR = "../configs/scenarios/"
+MODEL_BASE_DIR =  os.path.normpath("../models/")
+SCENARIO_BASE_DIR = os.path.normpath("../configs/scenarios/")
 
-def load_model(algo: str, scenario: str, timesteps: int):
+with open("../configs/eval.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+ALGORITHM = config["algo"].lower()
+SCENARIO = config["scenario"].lower()
+EPISODES = config["episodes"]
+TIMESTEPS = config["timesteps"]
+
+
+def load_model(algo: str, scenario: str, timesteps=TIMESTEPS):
     """
     Load the trained model based on algorithm, scenario, and timesteps.
     """
-    model_dir = os.path.join(MODEL_BASE_DIR, algo)
-    model_name = f"{algo}_{scenario}_{timesteps}_final.zip"
+    if scenario == "defend_the_center":
+        model_dir = os.path.join(MODEL_BASE_DIR, f"{algo}_defend_center")
+        model_name = f"{algo}_defend_center_{timesteps}_final.zip"
+    elif scenario == "deadly_corridor":
+        model_dir = os.path.join(MODEL_BASE_DIR, f"{algo}_deadly_corridor")
+        model_name = f"{algo}_deadly_corridor_{timesteps}_final.zip"
+    else:
+        raise ValueError(f"Unsupported scenario: {scenario}")
+
     model_path = os.path.join(model_dir, model_name)
 
     if not os.path.exists(model_path):
@@ -31,9 +47,18 @@ def load_model(algo: str, scenario: str, timesteps: int):
         raise ValueError(f"Unsupported algorithm: {algo}")
 
 
-def evaluate(model, scenario, episodes=10, render=True, resolution="1280x960"):
+def evaluate(model, scenario, episodes=EPISODES, render=True):
     """Evaluate a trained Doom agent."""
-
+    if scenario == "defend_the_center":
+        scenario_path = os.path.join(SCENARIO_BASE_DIR, "defend_the_center.cfg")
+        print(f"Loading scenario: {scenario_path}")
+        env = VizDoomGym(scenario_path=scenario_path,render=render)
+    elif scenario == "deadly_corridor":
+        scenario_path = os.path.join(SCENARIO_BASE_DIR, "deadly_corridor.cfg")
+        print(f"Loading scenario: {scenario_path}")
+        env = VizDoomGymCorridor(scenario_path=scenario_path,render=render)
+    else:
+        raise ValueError(f"Unsupported scenario: {scenario}")
 
     # Run evaluation episodes
     for ep in range(episodes):
@@ -49,25 +74,12 @@ def evaluate(model, scenario, episodes=10, render=True, resolution="1280x960"):
         print(f"Episode {ep + 1}: total_reward = {total_reward:.2f}")
 
     env.close()
+    print(f"Evaluation of {ALGORITHM} in {SCENARIO} has been completed!")
 
 
 def main():
-    # parser = argparse.ArgumentParser(description="Evaluate a trained VizDoom agent.")
-
-    # parser.add_argument("--algo", type=str, choices=["ppo", "a2c"], default="ppo",
-    #                     help="Algorithm used to train (ppo or a2c)")
-    # parser.add_argument("--scenario", type=str, default="defend_the_center",
-    #                     help="Scenario name (defend_the_center, deadly_corridor)")
-    # parser.add_argument("--timesteps", type=int, default=125000,
-    #                     help="Training step count used in filename (default: 125000)")
-    # parser.add_argument("--episodes", type=int, default=10,
-    #                     help="Number of episodes to evaluate")
-    # parser.add_argument("--headless", action="store_true",
-    #                     help="Disable rendering for faster evaluation (headless mode)")
-
-    # args = parser.parse_args()
-
-    # scenario_path = os.path.join(SCENARIO_BASE_DIR, f"{args.scenario}.cfg")
+    model = load_model(ALGORITHM, SCENARIO)
+    evaluate(model, SCENARIO)
 
 
 if __name__ == "__main__":
